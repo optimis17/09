2aac28713a15006e209e7713c98e31235e00183b
s <optimis17@sookmyung.ac.kr>
2020-10-29 15:44:45 +0900

    03-01

--- main.c
+++ main.c
@@ -5,19 +5,22 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i,average;
-	int sum;
+	int i;
 	int grade[SIZE];
-	sum=0;
+	int score[SIZE];
+	
 	for(i=0;i<SIZE;i++)
 	{
-		printf("학생 성적을 입력하세요: ");
-		scanf("%d",&grade[i]);
-		sum+=grade[i]; 
+		grade[i]=rand()%100;
+		score[i]=grade[i];
 	}
+
+		
 	
-	average=sum/SIZE;
-	printf("성적 평균:%d\n",average);
+	for(i=0;i<SIZE;i++)
+		printf("score[%d]=%d\n",i,score[i]);
+	
+
 	
 	
 	return 0;
